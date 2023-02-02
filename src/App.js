45d5a28b37adae076544f13c91032f5a9c853c53
neilk17 <kanakianeil@gmail.com>
2023-02-02 13:00:25 +0530

    ✨final commit

--- src/App.js
+++ src/App.js
@@ -23,23 +23,28 @@ function Header() {
   );
 }
 
-function Notification() {
+function Notification({ person, post }) {
   return (
       <>
-      <div class="flex gap-3 bg-gray-blue-1 p-4 rounded-lg mb-4">
+      <div className={`flex gap-3 ${post.isNew === true ? 'bg-gray-blue-1' : ''} p-4 rounded-lg mb-4`}>
         <img
-          className="rounded-full w-12 h-12"
-          src={user1.imageUrl}
-          alt={'Photo of ' + user1.name}
+          className="rounded-full w-12 h-12 hover:cursor-pointer"
+          src={person.imageUrl}
+          alt={'Photo of ' + person.name}
         />
         <div>
           <p class="text-gray-blue-5">
-            <span class="font-bold text-gray-blue-6">{user1.name}</span> 
-            &nbsp;{user1.action}&nbsp;
-            <span class="text-gray-blue-5 font-bold">{user1.post}</span>
-            {user1.unread? (<div className="inline-flex bg-red p-1 w-2 rounded-full ml-2"></div>) : (<></>)}
+            <span class="font-bold text-gray-blue-6 hover:cursor-pointer hover:text-blue">{person.name}</span> 
+            &nbsp;{post.action}&nbsp;
+            { 
+              post.type === 'message' ? 
+              (<div class="mt-2 border-2 border-gray-blue-2 hover:cursor-pointer hover:bg-gray-blue-2 rounded-md p-3">{post.title}</div> )
+              : 
+              (<span class="text-gray-blue-5 font-bold hover:cursor-pointer hover:text-blue">{post.title}</span> )
+            }
+            {post.isNew && <div className="inline-flex bg-red p-1 w-2 rounded-full ml-2" />}
           </p>
-          <p class="text-gray-blue-4">{user1.time}</p>
+          <p class="text-gray-blue-4">{post.time}</p>
         </div>
 
       </div>
@@ -47,43 +52,89 @@ function Notification() {
   );
 }
 
-{/* 
-  Angela Gray followed you
-  5m ago
-
-  Jacob Thompson has joined your group Chess Club
-  1 day ago
-
-  Rizky Hasanuddin sent you a private message
-  5 days ago
-  Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
-  I'm already having lots of fun and improving my game.
-
-  Kimberly Smith commented on your picture
-  1 week ago
-
-  Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
-  2 weeks ago
-
-  Anna Kim left the group Chess Club
-  2 weeks ago
-*/}
 function NotificationList() {
   const users = [
     { 
       name: 'Mark Webber', 
-      unread: true, 
+      isNew: true, 
       id: 1,
       action: 'reacted to your recent post',
-      post: 'My first tournament today!',
+      title: 'My first tournament today!',
       time: '1m ago',
-      imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+      imageUrl: '/images/avatar-mark-webber.webp',
     },
-    { name: 'Angela Gray', unread: false, id: 1 },
+    { 
+      name: 'Angela Gray', 
+      isNew: true, 
+      id: 2,
+      action: 'followed you',
+      time: '3m ago',
+      imageUrl: '/images/avatar-angela-gray.webp',
+    },
+    { 
+      name: 'Jacob Thompson', 
+      isNew: true, 
+      id: 3,
+      action: 'has joined your group',
+      title: 'Chess Club',
+      time: '1 day ago',
+      imageUrl: '/images/avatar-jacob-thompson.webp',
+    },
+    {
+      name: 'Rizky Hasanuddin ',
+      isNew: false,
+      id: 4,
+      type: 'message',
+      action: 'sent you a private message',
+      title: 'Hello, thanks for setting up the Chess Club. Ive been a member for a few weeks now and Im already having lots of fun and improving my game.',
+      time: '5 days ago', 
+      imageUrl: '/images/avatar-rizky-hasanuddin.webp',
+    },
+    {
+      name: 'Kimberly Smith ',
+      isNew: false,
+      id: 5,
+      type: 'picture',
+      action: 'commented on your picture',
+      title: '',
+      time: '1 week ago', 
+      imageUrl: '/images/avatar-kimberly-smith.webp',
+    },
+    {
+      name: 'Nathan Peterson ',
+      isNew: false,
+      id: 6,
+      action: 'reacted to your recent post',
+      title: '5 end-game strategies to increase your win rate',
+      time: '2 weeks ago', 
+      imageUrl: '/images/avatar-nathan-peterson.webp',
+    },
+    {
+      name: 'Anna Kim ',
+      isNew: false,
+      id: 7,
+      action: 'left the group ',
+      title: 'Chess Club',
+      time: '2 weeks ago', 
+      imageUrl: '/images/avatar-anna-kim.webp',
+    }
   ];
   const listItems = users.map(user =>
     <li key={user.id} >
-      <Notification />
+      <Notification 
+        person={{ 
+          name: user.name, 
+          imageUrl: user.imageUrl 
+          }}
+        post={
+          { 
+            isNew: user.isNew, 
+            action: user.action, 
+            type: user.type,
+            time: user.time,
+            title: user.title,
+          }}
+      />
     </li>
   );
 
